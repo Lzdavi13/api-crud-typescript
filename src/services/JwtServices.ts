8f84a9736e0e53f0df49003da0ee15c9908d706a
Luiz D O Santos <lzdavi1325@gmail.com>
2023-12-07 20:49:24 -0300

    feat: emplementado senha dotenv para senha jwt

--- src/services/JwtServices.ts
+++ src/services/JwtServices.ts
@@ -1,19 +1,14 @@
 import jwt from 'jsonwebtoken'
-
-const hash = '101120'
-
-// interface IJwtData {
-//   id: number
-// }
+import { env } from 'node:process'
 
 export const sign = (id: number) => {
-  const jwtToken = jwt.sign({ id }, hash, { expiresIn: '8h' })
+  const jwtToken = jwt.sign({ id }, env.JWT_HASH, { expiresIn: '8h' })
 
   return jwtToken
 }
 
 export const verify = (token: string) => {
-  const isValid = jwt.verify(token, hash)
+  const isValid = jwt.verify(token, env.JWT_HASH)
 
   return isValid
 }
